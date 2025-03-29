361f7dea9f7231de87456db2d3f013f0a72af786
Aloïs Fleury <fleuryalois@gmail.com>
2025-03-29 18:08:01 -0400

    corrections layout RoutineDetails et export d'un nouveau composant pour la priorité

--- app/src/main/java/ca/uqac/tp_mobile/presentation/components/RoutinePresentationPriorityField.kt
+++ app/src/main/java/ca/uqac/tp_mobile/presentation/components/RoutinePresentationPriorityField.kt
@@ -1,22 +1,28 @@
 package ca.uqac.tp_mobile.presentation.components
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.HorizontalDivider
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import ca.uqac.tp_mobile.R
+import ca.uqac.tp_mobile.presentation.Priority
 
 @Composable
-fun RoutinePresentationField(icon: @Composable () -> Unit, titleText: String, contentText: String) {
+fun RoutinePresentationPriorityField(priority: Priority) {
     Column(horizontalAlignment = Alignment.CenterHorizontally) {
         Row(
             verticalAlignment = Alignment.CenterVertically,
@@ -24,22 +30,30 @@ fun RoutinePresentationField(icon: @Composable () -> Unit, titleText: String, co
                 .padding(horizontal = 25.dp)
                 .fillMaxWidth()
         ) {
-            Spacer(modifier = Modifier.padding(horizontal = 15.dp, vertical = 40.dp))
-            icon()
-            Spacer(modifier = Modifier.padding(horizontal = 5.dp))
+            Spacer(
+                modifier = Modifier.padding(
+                    horizontal = 15.dp, vertical = 40.dp
+                )
+            )
+            Icon(
+                painter = painterResource(id = R.drawable.outline_priority_high),
+                contentDescription = "Priorité",
+                modifier = Modifier.size(24.dp),
+                tint = Color(0xFF000547)
+            )
             Text(
-                text = titleText,
+                text = "Priorité",
                 fontSize = 20.sp,
                 fontWeight = FontWeight.Bold,
                 color = Color(0xFF000547)
             )
         }
-        Text(
-            text = contentText,
-            fontSize = 18.sp,
-            color = Color(0xFF000547),
-        )
-        HorizontalDivider(thickness = 2.dp)
+        Box(Modifier.background(priority.type.backgroundColor)) {
+            Text(
+                text = priority.label,
+                fontSize = 18.sp,
+                color = Color(0xFF000547),
+            )
+        }
     }
-
 }
--- app/src/main/java/ca/uqac/tp_mobile/presentation/routineDetails/RoutineDetailsScreen.kt
+++ app/src/main/java/ca/uqac/tp_mobile/presentation/routineDetails/RoutineDetailsScreen.kt
@@ -44,6 +44,7 @@ import ca.uqac.tp_mobile.navigation.Screen
 import ca.uqac.tp_mobile.presentation.RoutineVM
 import ca.uqac.tp_mobile.presentation.components.BottomActionBarWithModification
 import ca.uqac.tp_mobile.presentation.components.RoutinePresentationField
+import ca.uqac.tp_mobile.presentation.components.RoutinePresentationPriorityField
 import ca.uqac.tp_mobile.presentation.formatDaysForLongDisplay
 import kotlinx.coroutines.flow.collectLatest
 
@@ -78,8 +79,8 @@ fun RoutineDetailsScreen(
 
     val primaryColor = Color(0xFF000547)
     val secondaryColor = Color(0xFFF4F4FB)
+
     selectedRoutine?.let { routine: RoutineVM ->
-        // TODO ? : utiliser la topbox du scaffold pour y mettre le bouton
         Scaffold(containerColor = primaryColor,
             snackbarHost = { SnackbarHost(snackbarHostState) },
             bottomBar = {
@@ -121,7 +122,8 @@ fun RoutineDetailsScreen(
                         .padding(outerPadding)
                         .padding(horizontal = 25.dp)
                         .clip(RoundedCornerShape(32.dp))
-                        .fillMaxSize(), containerColor = secondaryColor
+                        .fillMaxSize(),
+                    containerColor = secondaryColor
                 ) { innerPadding ->
                     Column(
                         modifier = Modifier
@@ -185,41 +187,7 @@ fun RoutineDetailsScreen(
                             }, titleText = "Lieu", contentText = routine.locationName
                         )
                         // TODO : exporter ça dans un composant propre ?
-                        Column {
-                            Row(
-                                verticalAlignment = Alignment.CenterVertically,
-                                modifier = Modifier
-                                    .padding(horizontal = 25.dp)
-                                    .fillMaxWidth()
-                            ) {
-                                Spacer(
-                                    modifier = Modifier.padding(
-                                        horizontal = 15.dp, vertical = 40.dp
-                                    )
-                                )
-                                Icon(
-                                    painter = painterResource(id = R.drawable.outline_priority_high),
-                                    contentDescription = "Priorité",
-                                    modifier = Modifier.size(24.dp),
-                                    tint = primaryColor
-                                )
-                                Text(
-                                    text = "Priorité",
-                                    fontSize = 20.sp,
-                                    fontWeight = FontWeight.Bold,
-                                    color = Color(0xFF000547)
-                                )
-                            }
-                            // FIXME : aligner le texte verticalement
-                            Box(Modifier.background(routine.priority.type.backgroundColor)) {
-                                Text(
-                                    text = routine.priority.label,
-                                    fontSize = 18.sp,
-                                    color = Color(0xFF000547),
-                                    modifier = Modifier.wrapContentHeight(align = Alignment.CenterVertically)
-                                )
-                            }
-                        }
+                        RoutinePresentationPriorityField(priority = routine.priority)
                     }
                 }
             }
